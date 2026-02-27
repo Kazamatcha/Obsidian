5023a5b6858105b664e0df0c8c787770a4dc2e18
Vhuy/matchalazada <darkvaica10@gmail.com>
2026-02-27 12:43:45 +0700

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -5925,7 +5925,7 @@ function Library:CreateWindow(WindowInfo)
     local Tabs
     local Container
 
-    local InitialLeftWidth = 130
+    local InitialLeftWidth = 160
     local IsCompact = WindowInfo.SidebarCompacted
     local LastExpandedWidth = InitialLeftWidth
 
